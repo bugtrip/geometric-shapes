16d9deab057c12c97c1dd6da5bb2d4714baf67f9
dev <dev.bg@yandex.com>
2025-03-22 23:02:31 +0300

    added class Point and class Square

--- src/geometric_shape.h
+++ src/geometric_shape.h
@@ -1,1 +1,35 @@
-123
+#ifndef _GEOMERTIC_H
+#define _GEOMETRIC_H
+#include <cstddef>
+
+enum Shape{square = 1, rectangle = 2};
+enum Calculations{sides, coords};
+
+class Point {
+private:
+    double x,y;
+public:
+    Point(double x = 0, double y = 0) : x(x), y(y) {}
+    double get_x();
+    double get_y();
+};
+
+class Square {
+private:
+    double a{0.0},b{0.0},c{0.0},d{0.0};
+    Calculations calc;
+    size_t length{0};
+public:
+    Square(Calculations calc, double a = 0.0, double b = 0.0, double c = 0.0, double d = 0.0) : calc(calc) {
+            set_side(a,b,c,d);
+    }
+    void set_side(double a, double b, double c, double d);
+    void get_side(double& a, double& b, double& c, double& d) const;
+    bool check_valid_sq(double a, double b, double c, double d);
+    double perimeter();
+    ~Square(){
+        delete [] pts;
+    }
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <iomanip>
+#include "geometric_shape.h"
 
 int main(){
-    double a,b,c,d;
-
+    int num_shape{0}; 
+    GeometricShape::show_menu();
+    
+    size_t count_pt{4};
+    Point* pts[count_pt] = {
+        new Point(1.9, 5.8),
+        new Point(2.9, 6.8),
+        new Point(3.9, 7.8),
+        new Point(4.9, 8.8)
+    };
+    
+       for(size_t i = 0; i < count_pt; ++i){
+        delete pts[i];
+    }
+    
     return 0;
 }
